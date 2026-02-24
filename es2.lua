63c1c585e3e681c95fd636813a8f42de68a6cbd5
iEmpathyy <ghibli991@gmail.com>
2026-02-24 14:03:16 +0800

    Update es2.lua

--- es2.lua
+++ es2.lua
@@ -2,7 +2,7 @@ local Workspace, RunService, Players, CoreGui, Lighting = cloneref(game:GetServi
 
 local ESP = {
     Enabled = true,
-    TeamCheck = true,
+    TeamCheck = false,
     MaxDistance = 1300,
     FontSize = 11,
     FadeOut = {
